e5091bb54fe6a2d212ad5e9d2c860af0786ddb82
SHUBHAM JANGAM <jangamshubham273@gmail.com>
2020-03-12 02:50:47 +0530

    IOS Static Smiley

---
+++ 10_Static_Smiley/GLESView.mm
@@ -0,0 +1,652 @@
+//
+//  Created by shubham_at_astromedicomp on 12/21/19.
+//  2D B&B
+//
+
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
+
+#import "GLESView.h"
+
+#import "vmath.h"
+
+enum
+{
+    AMC_ATTRIBUTE_POSITION = 0,
+    AMC_ATTRIBUTE_COLOR,
+    AMC_ATTRIBUTE_NORMAL,
+    AMC_ATTRIBUTE_TEXTURE0
+};
+
+@implementation GLESView
+{
+    EAGLContext *eaglContext;
+
+    GLuint defaultFramebuffer;
+    GLuint colorRenderbuffer;
+    GLuint depthRenderbuffer;
+
+    GLuint vertexShaderObject;
+    GLuint fragmentShaderObject;
+    GLuint shaderProgramObject;
+
+    GLuint vao_rectangle;
+    GLuint vbo_position_rectangle;
+    GLuint vbo_texture;
+
+    GLuint g_textSmiley;
+
+    GLuint samplerUniform;
+
+    GLuint mvpUniform;
+    vmath:: mat4 perspectiveProjectionMatrix;
+
+    id displayLink;
+    NSInteger animationFrameInterval;
+    BOOL isAnimating;
+
+    GLint width;
+    GLint height;
+}
+
+-(id)initWithFrame:(CGRect)frame
+{
+    // code
+    self = [super initWithFrame:frame];
+
+    if(self)
+    {
+        CAEAGLLayer *eaglLayer = (CAEAGLLayer *)super.layer;
+
+        eaglLayer.opaque = YES;
+        eaglLayer.drawableProperties=[NSDictionary dictionaryWithObjectsAndKeys:
+            [NSNumber numberWithBool:FALSE],
+            kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8,
+            kEAGLDrawablePropertyColorFormat, nil];
+
+        eaglContext = [[EAGLContext alloc]initWithAPI:kEAGLRenderingAPIOpenGLES3];
+        if(nil == eaglContext)
+        {
+            [self release];
+            return(nil);
+        }
+
+        [EAGLContext setCurrentContext:eaglContext];
+
+        glGenFramebuffers(1, &defaultFramebuffer);
+        glGenRenderbuffers(1, &colorRenderbuffer);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+
+        [eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer];
+
+        glFramebufferRenderbuffer(
+            GL_FRAMEBUFFER,
+            GL_COLOR_ATTACHMENT0,
+            GL_RENDERBUFFER,
+            colorRenderbuffer);
+
+        GLint backingHeight;
+        GLint backingWidth;
+
+        glGetRenderbufferParameteriv(
+            GL_RENDERBUFFER,
+            GL_RENDERBUFFER_WIDTH,
+            &backingWidth
+        );
+
+        glGetRenderbufferParameteriv(
+            GL_RENDERBUFFER,
+            GL_RENDERBUFFER_HEIGHT,
+            &backingHeight
+        );
+
+        glGenRenderbuffers(1, &depthRenderbuffer);
+        glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, backingWidth, backingHeight);
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+
+        if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        {
+            printf("Failed to create complete framebuffer object:  %x   \n",
+                glCheckFramebufferStatus(GL_FRAMEBUFFER));
+            glDeleteFramebuffers(1, &defaultFramebuffer);
+            glDeleteRenderbuffers(1, &colorRenderbuffer);
+            glDeleteRenderbuffers(1, &depthRenderbuffer);
+
+            return(nil);
+        }
+
+        printf("Renderer: %s | GL Version : %s | GLSL Version : %s\n",
+            glGetString(GL_RENDERER), glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+        isAnimating = NO;
+        animationFrameInterval = 60; // default since iOS 8.2
+        /*************************************************/
+
+        vertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+
+        const GLchar *vertexShaderSourceCode =
+            "#version 300 es" \
+            "\n" \
+            "in vec4 vPosition;" \
+            "in vec2 vTexture0_coord;" \
+            "out vec2 out_texture0_coord;" \
+            "uniform mat4 u_mvp_matrix;" \
+            "void main(void)" \
+            "{" \
+                "gl_Position = u_mvp_matrix * vPosition;" \
+                "out_texture0_coord = vTexture0_coord;" \
+            "}";
+
+        // specify above code of shader to vertext shader object
+        glShaderSource(vertexShaderObject,
+            1,
+            (const GLchar**)(&vertexShaderSourceCode),
+            NULL);
+
+        glCompileShader(vertexShaderObject);
+
+        // catching shader related errors if there are any
+        GLint iShaderCompileStatus = 0;
+        GLint iInfoLogLength = 0;
+        GLchar *szInfoLog = NULL;
+
+        // getting compile status code
+        glGetShaderiv(vertexShaderObject,
+            GL_COMPILE_STATUS,
+            &iShaderCompileStatus);
+
+        if(GL_FALSE == iShaderCompileStatus)
+        {
+            glGetShaderiv(vertexShaderObject, GL_INFO_LOG_LENGTH,
+                &iInfoLogLength);
+            if(iInfoLogLength > 0)
+            {
+                szInfoLog = (GLchar *)malloc(iInfoLogLength);
+                if(NULL != szInfoLog)
+                {
+                    GLsizei written;
+
+                    glGetShaderInfoLog(
+                        vertexShaderObject,
+                        iInfoLogLength,
+                        &written,
+                        szInfoLog
+                    );
+
+                    printf("VERTEX SHADER FATAL ERROR: %s\n", szInfoLog);
+                    free(szInfoLog);
+                    [self release];
+                }
+            }
+        }
+
+        // ***  Fragment Shader
+        // re-initialize
+        // catching shader related errors if there are any
+        iShaderCompileStatus = 0;
+        iInfoLogLength = 0;
+        szInfoLog = NULL;
+
+        fragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+        const GLchar *pcFragmentShaderSourceCode = 
+        "#version 300 es" \
+        "\n" \
+        "precision highp float;" \
+        "in vec2 out_texture0_coord;" \
+        "uniform sampler2D u_texture0_sampler;" \
+        "out vec4 vFragColor;" \
+        "void main(void)" \
+        "{" \
+            "vec3 tex = vec3(texture(u_texture0_sampler, out_texture0_coord));" \
+            "vFragColor = vec4(tex, 1.0f);" \
+        "}";
+
+        // specify above code of shader to vertext shader object
+        glShaderSource(fragmentShaderObject,
+            1,
+            (const GLchar**)&pcFragmentShaderSourceCode,
+            NULL);
+
+        // compile the vertext shader
+        glCompileShader(fragmentShaderObject);
+
+        // getting compile status code
+        glGetShaderiv(fragmentShaderObject,
+            GL_COMPILE_STATUS,
+            &iShaderCompileStatus);
+
+        if (GL_FALSE == iShaderCompileStatus)
+        {
+            glGetShaderiv(fragmentShaderObject,
+            GL_INFO_LOG_LENGTH,
+            &iInfoLogLength);
+
+        if (iInfoLogLength > 0)
+        {
+                szInfoLog = (GLchar *)malloc(iInfoLogLength);
+                if (NULL != szInfoLog)
+                {
+                    GLsizei written;
+
+                    glGetShaderInfoLog(
+                            fragmentShaderObject,
+                            iInfoLogLength,
+                            &written,
+                            szInfoLog
+                        );
+
+                    printf( ("FRAGMENT SHADER FATAL COMPILATION ERROR: %s\n"), szInfoLog);
+                    free(szInfoLog);
+                    [self release];
+                }
+            }
+        }
+
+        // create shader program objects
+        shaderProgramObject = glCreateProgram();
+
+        // attach fragment shader to shader program
+        glAttachShader(shaderProgramObject, vertexShaderObject);
+        glAttachShader(shaderProgramObject, fragmentShaderObject);
+
+        // Before Prelinking bind binding our no to vertex attribute
+        glBindAttribLocation(shaderProgramObject,
+            AMC_ATTRIBUTE_POSITION,
+            "vPosition");
+
+        glBindAttribLocation(shaderProgramObject,
+            AMC_ATTRIBUTE_TEXTURE0,
+            "vTexture0_coord");
+
+        // link the shader
+        glLinkProgram(shaderProgramObject);
+
+        GLint iShaderProgramLinkStatus = 0;
+        iInfoLogLength = 0;
+
+        glGetProgramiv(shaderProgramObject, GL_LINK_STATUS, &iShaderProgramLinkStatus);
+
+        if(GL_FALSE == iShaderProgramLinkStatus)
+        {
+            glGetProgramiv(shaderProgramObject, GL_LINK_STATUS,
+                &iInfoLogLength);
+
+            if(iInfoLogLength > 0)
+            {
+                szInfoLog = NULL;
+                szInfoLog = (char *)malloc(iInfoLogLength);
+                if(NULL != szInfoLog)
+                {
+                    GLsizei written;
+                    glGetProgramInfoLog(shaderProgramObject, iInfoLogLength,
+                        &written, szInfoLog);
+                    printf("Shader Program Link Log: %s \n", szInfoLog);
+                    free(szInfoLog);
+                    [self release];
+                }
+            }
+        }
+
+        // now this is rule: attribute binding should happen before linking program and
+        // uniforms binding should happen after linking
+        mvpUniform = glGetUniformLocation(
+            shaderProgramObject,
+            "u_mvp_matrix"
+        );
+
+            samplerUniform = glGetUniformLocation(
+                shaderProgramObject, "u_texture0_sampler");
+
+        g_textSmiley = [self loadTextureFromBMPFile:@"Smiley" :@"bmp"];
+
+
+        const GLfloat frectangleVertices[] = 
+        {
+            1.0f, 1.0f, 0.0f,
+            -1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f
+        };
+
+        const GLfloat ftextCooards[] = 
+        {
+            1.0f, 1.0f,
+            0.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f
+        };
+
+        glGenVertexArrays(1, &vao_rectangle);
+        glBindVertexArray(vao_rectangle);
+
+        glGenBuffers(1, &vbo_position_rectangle);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_position_rectangle);
+        glBufferData(GL_ARRAY_BUFFER,
+            sizeof(frectangleVertices),
+            frectangleVertices,
+            GL_STATIC_DRAW);
+
+        glVertexAttribPointer(
+            AMC_ATTRIBUTE_POSITION,
+            3,                              // how many co-ordinates in vertice
+            GL_FLOAT,                       // type of above data
+            GL_FALSE,                       // no normalization is desired
+            0,                              // (dangha)
+            NULL                            // offset to start in above attrib position
+        );
+
+        glEnableVertexAttribArray(AMC_ATTRIBUTE_POSITION);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        //
+        // working on colors of rectangle
+        //
+        glGenBuffers(1, &vbo_texture);
+        glBindBuffer(GL_ARRAY_BUFFER,
+        	vbo_texture);
+
+        glBufferData(GL_ARRAY_BUFFER,
+            sizeof(ftextCooards),
+            ftextCooards,
+            GL_STATIC_DRAW);
+
+        glVertexAttribPointer(AMC_ATTRIBUTE_TEXTURE0,
+            2,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            NULL);
+
+        glEnableVertexAttribArray(AMC_ATTRIBUTE_TEXTURE0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindVertexArray(0);
+
+        glEnable(GL_TEXTURE_2D);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        //glEnable(GL_CULL_FACE);
+        //glClearDepth(1.0f);
+
+        // set projection  Matrix
+        perspectiveProjectionMatrix = vmath::mat4::identity();
+
+        /*************************************************/
+        // GESTURE RECOGNITION
+        // Tap gesture code
+        UITapGestureRecognizer *singleTapGestureRecognizer=
+          [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector
+            (onSingleTap:)];
+
+        [singleTapGestureRecognizer setNumberOfTapsRequired:1];
+        [singleTapGestureRecognizer setNumberOfTouchesRequired:1];
+        [singleTapGestureRecognizer setDelegate:self];
+        [self addGestureRecognizer:singleTapGestureRecognizer];
+
+        UITapGestureRecognizer *doubleTapGestureRecognizer=
+          [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector
+            (onDoubleTap:)];
+
+        [doubleTapGestureRecognizer setNumberOfTapsRequired:2];
+        [doubleTapGestureRecognizer setNumberOfTouchesRequired:1];
+
+        [doubleTapGestureRecognizer setDelegate:self];
+        [self addGestureRecognizer:doubleTapGestureRecognizer];
+
+        [singleTapGestureRecognizer requireGestureRecognizerToFail:doubleTapGestureRecognizer];
+
+        // swipe gesture
+        UISwipeGestureRecognizer *swipeGestureRecognizer
+          = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(onSwipe:)];
+
+        [self addGestureRecognizer:swipeGestureRecognizer];
+
+        // long-press gesture
+        UILongPressGestureRecognizer *longPressGestureRecognizer =
+          [[UILongPressGestureRecognizer alloc]initWithTarget:self
+          action:@selector(onLongPress:)];
+
+        [self addGestureRecognizer:longPressGestureRecognizer];
+    }
+
+    return(self);
+}
+
+-(GLuint)loadTextureFromBMPFile:(NSString *)texFileName :(NSString *)extension
+{
+    NSString *textureFileNameWithPath = 
+        [[NSBundle mainBundle] pathForResource:texFileName ofType:extension];
+
+    UIImage *bmpImage =
+        [[UIImage alloc] initWithContentsOfFile:textureFileNameWithPath];
+
+    if(!bmpImage)
+    {
+        NSLog(@"can't find %@", textureFileNameWithPath);
+        return(0);
+    }
+
+    //CGImageRef cgImage = [bmpImage CGImageForProposedRect:nil context:nil hints:nil];
+    CGImageRef cgImage = bmpImage.CGImage;
+
+    int w = (int)CGImageGetWidth(cgImage);
+    int h = (int)CGImageGetHeight(cgImage);
+
+    CFDataRef imageData = CGDataProviderCopyData(CGImageGetDataProvider(cgImage));
+    void *pixels = (void *)CFDataGetBytePtr(imageData);
+
+    GLuint bmpTexture;
+    glGenTextures(1, &bmpTexture);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glBindTexture(GL_TEXTURE_2D, bmpTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    CFRelease(imageData);
+    return(bmpTexture);
+}
+
++(Class)layerClass
+{
+    // code
+    return([CAEAGLLayer class]);
+}
+
+-(void)drawView:(id)sender
+{
+    [EAGLContext setCurrentContext:eaglContext];
+
+    glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebuffer);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glUseProgram(shaderProgramObject);
+
+    vmath::mat4 modelViewMatrix = vmath::mat4::identity();
+    vmath::mat4 modelRotationMatrix = vmath::mat4::identity();
+    vmath::mat4 modelViewProjectionMatrix = vmath::mat4::identity();
+
+    modelViewMatrix = vmath::translate(0.0f, 0.0f, -4.0f);
+
+    modelViewProjectionMatrix = perspectiveProjectionMatrix * modelViewMatrix;
+
+    // uniforms are given to m_uv_matrix (i.e. model view matrix)
+    glUniformMatrix4fv(
+            mvpUniform,
+            1,			//	how many matrices
+            GL_FALSE,	//	Transpose is needed ? ->
+            modelViewProjectionMatrix
+        );
+
+    glBindTexture(GL_TEXTURE_2D, g_textSmiley);
+    glBindVertexArray(vao_rectangle);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glBindVertexArray(0);
+
+    glUseProgram(0);
+
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+    [eaglContext presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+-(void)layoutSubviews // this is like redraw
+{
+    GLfloat fWidth;
+    GLfloat fHeight;
+
+    // code
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderbuffer);
+    [eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width);
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height);
+
+    glGenRenderbuffers(1, &depthRenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+
+    if(0 == height)
+    {
+        height = 1;
+    }
+
+    glViewport(0, 0, width, height);
+
+    fWidth = (GLfloat)width;
+    fHeight = (GLfloat)height;
+
+    perspectiveProjectionMatrix = vmath::perspective(
+            45.0f, fWidth/fHeight, 0.1f, 100.0f
+        );
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        printf("Failed To Create Complete Framebuffer Object %x\n",
+            glCheckFramebufferStatus(GL_FRAMEBUFFER));
+    }
+
+    [self drawView:nil];
+}
+
+-(void)startAnimation
+{
+    if(!isAnimating)
+    {
+        displayLink = [
+            NSClassFromString(@"CADisplayLink") displayLinkWithTarget:self selector:@selector(drawView:)];
+            [displayLink setPreferredFramesPerSecond:animationFrameInterval];
+            [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+
+    isAnimating:YES;
+    }
+}
+
+-(void)stopAnimation
+{
+    if(isAnimating)
+    {
+        [displayLink invalidate];
+        displayLink = nil;
+
+        isAnimating = NO;
+    }
+}
+
+//
+
+-(BOOL)acceptsFirstResponder
+{
+    // code
+    return(YES);
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+
+}
+
+- (void)onSingleTap:(UITapGestureRecognizer *)gr
+{
+    // code
+    // [self setNeedsDisplay]; // repainting
+}
+
+- (void)onDoubleTap:(UITapGestureRecognizer *)gr
+{
+    // code
+    // [self setNeedsDisplay]; // repainting
+}
+
+- (void)onSwipe:(UISwipeGestureRecognizer *)gr
+{
+    // code
+    [self release];
+    exit(0);
+}
+
+- (void)onLongPress:(UILongPressGestureRecognizer *)gr
+{
+    // code
+    // [self setNeedsDisplay]; // repainting
+}
+
+- (void)dealloc
+{
+
+    if(vbo_position_rectangle)
+    {
+        glDeleteBuffers(1, &vbo_position_rectangle);
+        vbo_position_rectangle = 0;
+    }
+
+    if(vbo_texture)
+    {
+        glDeleteBuffers(1, &vbo_texture);
+        glDeleteTextures(1, &g_textSmiley);
+        g_textSmiley = 0;
+        vbo_texture = 0;
+    }
+
+
+    if (vao_rectangle)
+    {
+        glDeleteVertexArrays(1, &vao_rectangle);
+        vao_rectangle = 0;
+    }
+
+    if(depthRenderbuffer)
+    {
+        glDeleteRenderbuffers(1, &depthRenderbuffer);
+        depthRenderbuffer = 0;
+    }
+
+    if(colorRenderbuffer)
+    {
+        glDeleteRenderbuffers(1, &colorRenderbuffer);
+        colorRenderbuffer = 0;
+    }
+
+    if(defaultFramebuffer)
+    {
+        glDeleteFramebuffers(1, &defaultFramebuffer);
+        defaultFramebuffer = 0;
+    }
+
+    if([EAGLContext currentContext] == eaglContext)
+    {
+        [EAGLContext setCurrentContext:nil];
+    }
+
+    [eaglContext release];
+    eaglContext = nil;
+
+    [super dealloc];
+}
+
+@end

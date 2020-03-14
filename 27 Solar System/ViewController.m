fddd3aec191d745f471afe50cfe3e132dc033c1b
SHUBHAM JANGAM <jangamshubham273@gmail.com>
2020-03-14 14:38:14 +0530

    IOS Solar System

---
+++ 27 Solar System/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Window
+//
+//  Created by shubham_at_astromedicomp on 12/21/19.
+//
+
+#import "ViewController.h"
+
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
